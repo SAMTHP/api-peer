e55542f53756a830d662055ae472c4ae245cdc65
Samir Founou <samir.founou@avizzeo.com>
2020-10-16 13:56:25 +0200

    MAJ

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,25 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\ApiPeerInfo;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiController extends AbstractController
 {
     /**
      * @Route("/api/set-user-a", name="api_set_user_a", methods={"POST"})
      */
-    public function index(SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, Request $request)
+    public function setUserA(SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, Request $request)
     {
         $data = $request->getContent();
         $decodeData = \json_decode($data);
 
-        dd($decodeData->user_a);
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
-        ]);
+        $apiPeer = new ApiPeerInfo();
+        $apiPeer->setNameConversation($decodeData->name_conversation);
+        $apiPeer->setUserA($decodeData->user_a);
+
+        $result = $serializerInterface->serialize(
+            $apiPeer,
+            'json'
+        );
+
+        return new JsonResponse(
+            $result,
+            Response::HTTP_OK,
+            [],
+            true
+        );
     }
 }
