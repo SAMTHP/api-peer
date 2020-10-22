5d32995e2b03ad4d5a3ccac2ebee42613deaf408
Samir Founou <samir.founou@avizzeo.com>
2020-10-22 11:01:27 +0200

    MAJ

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\ApiPeerInfo;
+use App\Repository\ApiPeerInfoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +25,33 @@ class ApiController extends AbstractController
         $apiPeer = new ApiPeerInfo();
         $apiPeer->setNameConversation($decodeData->name_conversation);
         $apiPeer->setUserA([$decodeData->user_a]);
+
+        $entityManagerInterface->persist($apiPeer);
+        $entityManagerInterface->flush();
+        
+        $result = $serializerInterface->serialize(
+            $apiPeer,
+            'json'
+        );
+
+        return new JsonResponse(
+            $result,
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+
+    /**
+     * @Route("/api/set-user-b", name="api_set_user_b", methods={"POST"})
+     */
+    public function setUserB(ApiPeerInfoRepository $apiPeerInfoRepository, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, Request $request)
+    {
+        $data = $request->getContent();
+        $decodeData = \json_decode($data);
+
+        $apiPeer = $apiPeerInfoRepository->findByNameConversation($decodeData->name_conversation);
+        $apiPeer->setUserB([$decodeData->user_b]);
 
         $entityManagerInterface->persist($apiPeer);
         $entityManagerInterface->flush();
